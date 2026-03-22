d961425c79539e77a3d844a68eda495d49f2f7ec
rafaelmcuenya <rafael.mcuenya@uel.br>
2026-03-22 10:43:34 -0300

    Update hashfile.h

--- include/hashfile.h
+++ include/hashfile.h
@@ -1,10 +1,74 @@
 #ifndef HASHFILE_H
+#define HASHFILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /*
    Este TAD contém a documentação do hashfile utilizado no projeto. Um hashfile é ...
 */
 
-typedef void* ???;
+typedef void* Bucket;
+typedef void* Diretorio;
+typedef void* Hash;
+typedef void* Dados;
+
+#define BUCKET_SIZE 4
+
+
+Hash* createHash();
+/*
+Função para criar o Hashfile
+*/
+
+void inserirItemHash(Hash *eh, int key, int value);
+/*
+
+*/
+
+int  buscaItemHash(Hash *eh, int key, int *value);
+/*
+
+*/
+
+void deletarItemHash(Hash *eh, int key);
+/*
+
+*/
+
+/*
+ int ??hash(int key);
+necessário??
+*/
+
+void dobraDirectorio(Hash *eh);
+/*
+
+*/
+
+void splitBucket(Hash *eh, int index);
+/*
+
+*/
+
+Bucket* createBucket(int depth);
+/*
+
+*/
+
+// Apagar dps
+void eh_print(Hash *eh);
+/*
+
+*/
+
+
+void freeHash(Hash *eh);
+/*
+
+*/
+
+#endif
 
 
 #endif
