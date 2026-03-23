c59595a53c291bb3eeac631a596529f33e421b3c
rafaelmcuenya <rafael.mcuenya@uel.br>
2026-03-23 10:31:23 -0300

    Update Testhashfile.c

--- test/Testhashfile.c
+++ test/Testhashfile.c
@@ -3,3 +3,16 @@
 
 void setUp(void) {}
 void setDowm(void) {}
+
+
+void test_NomeDaFuncao(void){
+  TEST_ASSERT_RESTO();
+}
+
+int main (void){
+UNITY_BEGIN();
+
+RUN_TEST(test_NomeDaFuncao);
+
+return UNITY_END();
+}
