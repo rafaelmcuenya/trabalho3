4b7d0b99643d8de104309b285db3e3b80cb39cae
rafaelmcuenya <rafael.mcuenya@uel.br>
2026-03-23 09:09:50 -0300

    Update hashfile.h

--- include/hashfile.h
+++ include/hashfile.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /*
-   Este TAD contém a documentação do hashfile extensível utilizado no projeto. 
+   Este TAD contém a documentação do hashfile extensível. 
    Um hashfile extensível é um método de armazenamento em dispositivos de memória secundários, tendo como exemplo mais notável, o disco rígido.
    Permite gerenciar de forma dinÂmica o espalhamento a depender se necessita dimunuir/aumentar o espaço reservado, de forma automática.
    
@@ -21,22 +21,25 @@ typedef void* Dados;
 
 Hash* createHash();
 /*
-Função para criar o hashfile
+Função para criar o hashfile. 
+Retorna um ponteiro para o hashfile, ou NULL em caso de erro.
 */
 
 void inserirItemHash(Hash *???, int chaveH, int info);
 /*
-Função para inserir um item no hasfile
+Função para inserir um item no hasfile. 
 */
 
 int  buscaItemHash(Hash *???, int chaveH, int *info);
 /*
-Função para procurar um item específica por meio de sua chave.
+Função para procurar um item específica por meio de sua chave. 
+Retorna o ponteiro do item caso encontre, retorna 0 caso contrário.
 */
 
 void deletarItemHash(Hash *???, int chaveH);
 /*
-Função para deletar um item por meio da chave
+Função para deletar um item por meio da chave. 
+Retorna 1 caso sucedido, 0 caso contrário.
 */
 
 /*
@@ -46,17 +49,21 @@ necessário??
 
 void dobraDiretorio(Hash *???);
 /*
-Função para dobrar a capacidade do diretório
+Função para dobrar a capacidade do diretório. 
+Retorna 1 caso operação tenha sido bem sucedida, 0 em caso de falha.
 */
 
 void splitBucket(Hash *???, int index);
 /*
-Função para dividir e redistribuir automaticamente um bucket
+Função para dividir e redistribuir automaticamente um bucket. 
+Retorna 1 caso tenha sucesso, 0 caso falhe.
 */
 
 Bucket* createBucket(int nivel);
 /*
-Função para criar um novo bucket na profundidade repassada pelo parâmetro
+Função para criar um novo bucket na profundidade repassada pelo parâmetro. 
+Retorna um ponteiro para tal bucket caso criado corretamente.
+Retorna NULL caso ocorra algum erro.
 */
 
 // Apagar dps
