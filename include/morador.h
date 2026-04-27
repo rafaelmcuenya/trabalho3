07c4a67b44ebe2d5d371af484c60345a4a8c54b5
rafaelmcuenya <rafael.mcuenya@uel.br>
2026-04-27 11:21:59 -0300

    Update and rename casas.h to morador.h
    Continuar posteriormente

---
+++ include/morador.h
@@ -0,0 +1,20 @@
+#ifndef MORADOR_H
+#define MORADOR_H
+
+#include <stddef.h>
+
+/*
+   Um morador é um habitante que mora em alguma das quadras.
+   
+   Todas as operações utilizadas para um habutante são válidas para um morador,
+   visto que todo morador é um habitante de Bitnópolis, mas nem todo habitante é um morador,
+   como por exemplo, os sem-teto.
+
+   Este módulo contém funções específicas para um morador, envolvendo principalmente elementos
+   envolvendo o endereço, como face, número, dentre outros.
+*/
+
+typedef void* Morador;
+
+
+#endif
