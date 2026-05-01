e9fbbb4135b1c6d500721811986bde906e27dd58
rafaelmcuenya <rafael.mcuenya@uel.br>
2026-05-01 16:24:59 -0300

    Update t_hashfile.c

--- tst/t_hashfile.c
+++ tst/t_hashfile.c
@@ -3,382 +3,258 @@
 #include <stdlib.h>
 #include <string.h>
 
-void setUp(void){}
-void tearDown(void){}
+static HashFile *hf;
+static const char *nomeBase = "teste_hashfile";
 
-void testCriarHash1(void){
-    HashFile hf = createHashFile(4, 1);
+void setUp(void) {
+    hf = criarHF(nomeBase, 4, 2);
+}
+
+void tearDown(void) {
+    if (hf) {
+        freeHF(hf);
+        hf = NULL;
+    }
+}
+
+void test_criarHF_sucesso(void) {
     TEST_ASSERT_NOT_NULL(hf);
-    freeHash(hf);
+    TEST_ASSERT_EQUAL_INT(2, getProfGlobalHF(hf));
+    TEST_ASSERT_EQUAL_INT(4, getTamBucketHF(hf));
 }
 
-void testCriarHash2(void){
-    HashFile hf = createHashFile(0, 1);
-    TEST_ASSERT_NULL(hf);
+void test_criarHF_parametros_invalidos(void) {
+    HashFile *h = criarHF(NULL, 4, 2);
+    TEST_ASSERT_NULL(h);
+
+    h = criarHF("teste", 0, 2);
+    TEST_ASSERT_NULL(h);
+
+    h = criarHF("teste", 4, 0);
+    TEST_ASSERT_NULL(h);
 }
 
-void testCriarHash3(void){
-    HashFile hf = createHashFile(4, 0);
-    TEST_ASSERT_NULL(hf);
+void test_insertHF_sucesso(void) {
+    char dado[] = "dados_teste_123";
+    int ret = insertHF(hf, "chave1", dado, strlen(dado) + 1);
+    TEST_ASSERT_EQUAL_INT(1, ret);
+    TEST_ASSERT_EQUAL_INT(1, (int)totalRegistrosHF(hf));
 }
 
-void testCriarHash4(void){
-    HashFile hf = createHashFile(4, 3);
-    TEST_ASSERT_NOT_NULL(hf);
-    int profundidade = getProfundidadeGlobal(hf);
-    TEST_ASSERT_EQUAL_INT(3, profundidade);
-    freeHash(hf);
+void test_insertHF_chave_duplicada(void) {
+    char dado[] = "dados_teste";
+    insertHF(hf, "chave1", dado, strlen(dado) + 1);
+    int ret = insertHF(hf, "chave1", dado, strlen(dado) + 1);
+    TEST_ASSERT_EQUAL_INT(0, ret);
+    TEST_ASSERT_EQUAL_INT(1, (int)totalRegistrosHF(hf));
 }
 
-void testCriarHash5(void){
-    HashFile hf = createHashFile(4, 2);
-    TEST_ASSERT_NOT_NULL(hf);
-    int tamanho = getTamanhoDiretorio(hf);
-    TEST_ASSERT_EQUAL_INT(4, tamanho); 
-    freeHash(hf);
+void test_insertHF_parametros_invalidos(void) {
+    char dado[] = "dados";
+    TEST_ASSERT_EQUAL_INT(-1, insertHF(NULL, "chave", dado, 5));
+    TEST_ASSERT_EQUAL_INT(-1, insertHF(hf, NULL, dado, 5));
+    TEST_ASSERT_EQUAL_INT(-1, insertHF(hf, "chave", NULL, 5));
+    TEST_ASSERT_EQUAL_INT(-1, insertHF(hf, "chave", dado, 0));
 }
 
-void testCriarHash6(void){
-    HashFile hf = createHashFile(8, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    int tamanhoBucket = getTamanhoBucket(hf);
-    TEST_ASSERT_EQUAL_INT(8, tamanhoBucket);
-    freeHash(hf);
+void test_buscaHF_encontrado(void) {
+    char dado[] = "dados_busca";
+    insertHF(hf, "chaveX", dado, strlen(dado) + 1);
+
+    char buffer[64];
+    sizeT tam = sizeof(buffer);
+    int ret = buscaHF(hf, "chaveX", buffer, &tam);
+    TEST_ASSERT_EQUAL_INT(1, ret);
+    TEST_ASSERT_EQUAL_STRING("dados_busca", buffer);
 }
 
-void testCriarHash6(void){
-    HashFile hf = createHashFile(8, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    int tamanhoBucket = hashGetBucketSize? 
-    freeHash(hf);
+void test_buscaHF_nao_encontrado(void) {
+    char buffer[64];
+    sizeT tam = sizeof(buffer);
+    int ret = buscaHF(hf, "inexistente", buffer, &tam);
+    TEST_ASSERT_EQUAL_INT(0, ret);
 }
 
-
-
-void testInserirItem(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    int resultado = inserirItemHash(hf, 10, 100);
-    TEST_ASSERT_EQUAL_INT(1, resultado);
-    
-    freeHash(hf);
+void test_buscaHF_parametros_invalidos(void) {
+    char buffer[64];
+    sizeT tam = sizeof(buffer);
+    TEST_ASSERT_EQUAL_INT(-1, buscaHF(NULL, "chave", buffer, &tam));
+    TEST_ASSERT_EQUAL_INT(-1, buscaHF(hf, NULL, buffer, &tam));
 }
 
-void testBuscaItem1(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    
-    int valor;
-    int encontrado = buscaItemHash(hf, 10, &valor);
-    TEST_ASSERT_EQUAL_INT(1, encontrado);
-    TEST_ASSERT_EQUAL_INT(100, valor);
-    
-    freeHash(hf);
+void test_buscaHF_buffer_insuficiente(void) {
+    char dado[] = "dados_grandes";
+    insertHF(hf, "chaveG", dado, strlen(dado) + 1);
+
+    char buffer[5];
+    sizeT tam = 5;
+    int ret = buscaHF(hf, "chaveG", buffer, &tam);
+    TEST_ASSERT_EQUAL_INT(-1, ret);
 }
 
-void testBuscaItem2(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    
-    int valor;
-    int encontrado = buscaItemHash(hf, 99, &valor);
-    TEST_ASSERT_EQUAL_INT(0, encontrado);
-    
-    freeHash(hf);
+void test_deletarItemHF_sucesso(void) {
+    char dado[] = "para_deletar";
+    insertHF(hf, "chaveDel", dado, strlen(dado) + 1);
+    TEST_ASSERT_EQUAL_INT(1, (int)totalRegistrosHF(hf));
+
+    int ret = deletarItemHF(hf, "chaveDel");
+    TEST_ASSERT_EQUAL_INT(1, ret);
+    TEST_ASSERT_EQUAL_INT(0, (int)totalRegistrosHF(hf));
 }
 
-void testBuscaItem3(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    
-    int encontrado = buscaItemHash(hf, 10, NULL);
-    TEST_ASSERT_EQUAL_INT(1, encontrado);
-    
-    freeHash(hf);
+void test_deletarItemHF_nao_encontrado(void) {
+    int ret = deletarItemHF(hf, "inexistente");
+    TEST_ASSERT_EQUAL_INT(0, ret);
 }
 
-void testInserirItens(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-
-    int resultado1 = inserirItemHash(hf, 10, 100);
-    int resultado2 = inserirItemHash(hf, 20, 200);
-    int resultado3 = inserirItemHash(hf, 30, 300);
-    int resultado4 = inserirItemHash(hf, 40, 400);
-    
-    TEST_ASSERT_EQUAL_INT(1, resultado1);
-    TEST_ASSERT_EQUAL_INT(1, resultado2);
-    TEST_ASSERT_EQUAL_INT(1, resultado3);
-    TEST_ASSERT_EQUAL_INT(1, resultado4);
-    
-    int quantidade = getTotalElementos(hf);
-    TEST_ASSERT_EQUAL_INT(4, quantidade);
-    
-    freeHash(hf);
+void test_deletarItemHF_parametros_invalidos(void) {
+    TEST_ASSERT_EQUAL_INT(-1, deletarItemHF(NULL, "chave"));
+    TEST_ASSERT_EQUAL_INT(-1, deletarItemHF(hf, NULL));
 }
 
-void testInserirItemChave(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    inserirItemHash(hf, 10, 999);
-    
-    int valor;
-    buscaItemHash(hf, 10, &valor);
-    TEST_ASSERT_EQUAL_INT(999, valor);
-    
-    freeHash(hf);
+void test_refreshHF_sucesso(void) {
+    char dado[] = "original";
+    insertHF(hf, "chaveRef", dado, strlen(dado) + 1);
+
+    char novo[] = "atualizado";
+    int ret = refreshHF(hf, "chaveRef", novo, strlen(novo) + 1);
+    TEST_ASSERT_EQUAL_INT(1, ret);
+
+    char buffer[64];
+    sizeT tam = sizeof(buffer);
+    buscaHF(hf, "chaveRef", buffer, &tam);
+    TEST_ASSERT_EQUAL_STRING("atualizado", buffer);
 }
 
-
-
-void testDeletarItem1(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    
-    int resultado = deletarItemHash(hf, 10);
-    TEST_ASSERT_EQUAL_INT(1, resultado);
-    
-    freeHash(hf);
+void test_refreshHF_nao_encontrado(void) {
+    char dado[] = "dado";
+    int ret = refreshHF(hf, "inexistente", dado, strlen(dado) + 1);
+    TEST_ASSERT_EQUAL_INT(0, ret);
 }
 
-void testDeletarItem2(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    deletarItemHash(hf, 10);
-    
-    int valor;
-    int encontrado = buscaItemHash(hf, 10, &valor);
-    TEST_ASSERT_EQUAL_INT(0, encontrado);
-    
-    freeHash(hf);
+void test_refreshHF_parametros_invalidos(void) {
+    char dado[] = "dado";
+    TEST_ASSERT_EQUAL_INT(-1, refreshHF(NULL, "chave", dado, 5));
+    TEST_ASSERT_EQUAL_INT(-1, refreshHF(hf, NULL, dado, 5));
+    TEST_ASSERT_EQUAL_INT(-1, refreshHF(hf, "chave", NULL, 5));
+    TEST_ASSERT_EQUAL_INT(-1, refreshHF(hf, "chave", dado, 0));
 }
 
-void testDeletarItem3(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    
-    int resultado = deletarItemHash(hf, 99);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-    
-    freeHash(hf);
+void test_totalRegistrosHF(void) {
+    TEST_ASSERT_EQUAL_INT(0, (int)totalRegistrosHF(hf));
+
+    char dado[] = "dados";
+    insertHF(hf, "c1", dado, strlen(dado) + 1);
+    insertHF(hf, "c2", dado, strlen(dado) + 1);
+    insertHF(hf, "c3", dado, strlen(dado) + 1);
+    TEST_ASSERT_EQUAL_INT(3, (int)totalRegistrosHF(hf));
+
+    deletarItemHF(hf, "c2");
+    TEST_ASSERT_EQUAL_INT(2, (int)totalRegistrosHF(hf));
 }
 
-void testDeletarItem4(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    int resultado = deletarItemHash(hf, 10);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-    
-    freeHash(hf);
+void test_totalRegistrosHF_NULL(void) {
+    TEST_ASSERT_EQUAL_INT(0, (int)totalRegistrosHF(NULL));
 }
 
-
-
-void testSplit1(void){
-    HashFile hf = createHashFile(2, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    inserirItemHash(hf, 20, 200);
-    inserirItemHash(hf, 30, 300);
-    
-    int valor;
-    int encontrado = buscaItemHash(hf, 30, &valor);
-    TEST_ASSERT_EQUAL_INT(1, encontrado);
-    TEST_ASSERT_EQUAL_INT(300, valor);
-    
-    freeHash(hf);
+void test_getProfGlobalHF(void) {
+    TEST_ASSERT_EQUAL_INT(2, getProfGlobalHF(hf));
+    TEST_ASSERT_EQUAL_INT(-1, getProfGlobalHF(NULL));
 }
 
-void testSplit2(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    inserirItemHash(hf, 20, 200);
-    inserirItemHash(hf, 30, 300);
-    inserirItemHash(hf, 40, 400);
-    
-    int resultado = splitBucket(hf, 0);
-    TEST_ASSERT_EQUAL_INT(1, resultado);
-    
-    freeHash(hf);
+void test_getTamBucketHF(void) {
+    TEST_ASSERT_EQUAL_INT(4, getTamBucketHF(hf));
+    TEST_ASSERT_EQUAL_INT(-1, getTamBucketHF(NULL));
 }
 
-void testDobraDiretorio(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    int profundidadeAntes = getProfundidadeGlobal(hf);
-    TEST_ASSERT_EQUAL_INT(1, profundidadeAntes);
-    
-    int resultado = dobraDiretorio(hf);
-    TEST_ASSERT_EQUAL_INT(1, resultado);
-    
-    int profundidadeDepois = getProfundidadeGlobal(hf);
-    TEST_ASSERT_EQUAL_INT(2, profundidadeDepois);
-    
-    int tamanhoDiretorio = getTamanhoDiretorio(hf);
-    TEST_ASSERT_EQUAL_INT(4, tamanhoDiretorio);
-    
-    freeHash(hf);
+void test_getInfoBucketHF(void) {
+    int qtd, prof;
+    int ret = getInfoBucketHF(hf, 0, &qtd, &prof);
+    TEST_ASSERT_EQUAL_INT(1, ret);
+    TEST_ASSERT_EQUAL_INT(0, qtd);
+    TEST_ASSERT_EQUAL_INT(2, prof);
 }
 
-
-
-void testCriaBucket1(void){
-    Bucket b = createBucket(1);
-    TEST_ASSERT_NOT_NULL(b);
+void test_getInfoBucketHF_invalido(void) {
+    int qtd, prof;
+    TEST_ASSERT_EQUAL_INT(0, getInfoBucketHF(NULL, 0, &qtd, &prof));
+    TEST_ASSERT_EQUAL_INT(0, getInfoBucketHF(hf, -1, &qtd, &prof));
+    TEST_ASSERT_EQUAL_INT(0, getInfoBucketHF(hf, 999, &qtd, &prof));
 }
 
-void testCriaBucket2(void){
-    Bucket b = createBucket(-1);
-    TEST_ASSERT_NULL(b);
+void test_insercao_multipla_com_split(void) {
+    char dado[32];
+    for (int i = 1; i <= 20; i++) {
+        char chave[16];
+        snprintf(chave, sizeof(chave), "chave%d", i);
+        snprintf(dado, sizeof(dado), "dados_numero_%d", i);
+        insertHF(hf, chave, dado, strlen(dado) + 1);
+    }
+    TEST_ASSERT_EQUAL_INT(20, (int)totalRegistrosHF(hf));
+
+    char buffer[64];
+    sizeT tam = sizeof(buffer);
+    int ret = buscaHF(hf, "chave10", buffer, &tam);
+    TEST_ASSERT_EQUAL_INT(1, ret);
+    TEST_ASSERT_EQUAL_STRING("dados_numero_10", buffer);
 }
 
+void test_deletar_e_reinserir(void) {
+    char dado[] = "primeiro";
+    insertHF(hf, "chaveX", dado, strlen(dado) + 1);
+    deletarItemHF(hf, "chaveX");
+    TEST_ASSERT_EQUAL_INT(0, (int)totalRegistrosHF(hf));
 
+    char dado2[] = "segundo";
+    int ret = insertHF(hf, "chaveX", dado2, strlen(dado2) + 1);
+    TEST_ASSERT_EQUAL_INT(1, ret);
+    TEST_ASSERT_EQUAL_INT(1, (int)totalRegistrosHF(hf));
 
-
-void testFreeHash1(void){
-    freeHash(NULL);
-    TEST_ASSERT_TRUE(1); 
+    char buffer[64];
+    sizeT tam = sizeof(buffer);
+    buscaHF(hf, "chaveX", buffer, &tam);
+    TEST_ASSERT_EQUAL_STRING("segundo", buffer);
 }
 
-void testFreeHash2(void){
-    HashFile hf = createHashFile(4, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    
-    inserirItemHash(hf, 10, 100);
-    inserirItemHash(hf, 20, 200);
-    inserirItemHash(hf, 30, 300);
-    
-    freeHash(hf);
-    TEST_ASSERT_TRUE(1); 
+void test_gerarDumpHF(void) {
+    char dado[] = "dump_test";
+    insertHF(hf, "chaveDump", dado, strlen(dado) + 1);
+    int ret = gerarDumpHF(hf);
+    TEST_ASSERT_EQUAL_INT(1, ret);
 }
 
-void testGetProfundidadeBucket(void){
-    HashFile hf = createHashFile(4, 2);
-    TEST_ASSERT_NOT_NULL(hf);
-    int profundidade = getProfundidadeBucket(hf, 0);
-    TEST_ASSERT_EQUAL_INT(2, profundidade);
-    freeHash(hf);
+void test_gerarDumpHF_NULL(void) {
+    TEST_ASSERT_EQUAL_INT(0, gerarDumpHF(NULL));
 }
 
-void testGetQuantidadeBucket(void){
-    HashFile hf = createHashFile(4, 2);
-    TEST_ASSERT_NOT_NULL(hf);
-    inserirItemHash(hf, 10, 100);
-    int quantidade = getQuantidadeBucket(hf, 0);
-    TEST_ASSERT_EQUAL_INT(1, quantidade);
-    freeHash(hf);
-}
-
-void testBucketEstaCheio(void){
-    HashFile hf = createHashFile(2, 1);
-    TEST_ASSERT_NOT_NULL(hf);
-    inserirItemHash(hf, 10, 100);
-    TEST_ASSERT_EQUAL_INT(0, bucketEstaCheio(hf, 0));
-    inserirItemHash(hf, 20, 200);
-    TEST_ASSERT_EQUAL_INT(1, bucketEstaCheio(hf, 0));
-    freeHash(hf);
-}
-
-void testErro1(void){
-    int resultado = inserirItemHash(NULL, 10, 100);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-}
-
-void testErro2(void){
-    int valor;
-    int resultado = buscaItemHash(NULL, 10, &valor);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-}
-
-void testErro3(void){
-    int resultado = deletarItemHash(NULL, 10);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-}
-
-void testErro4(void){
-    int resultado = dobraDiretorio(NULL);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-}
-
-void testErro5(void){
-    int resultado = splitBucket(NULL, 0);
-    TEST_ASSERT_EQUAL_INT(0, resultado);
-}
-
-void testErro6(void){
-    int resultado = getProfundidadeGlobal(NULL);
-    TEST_ASSERT_EQUAL_INT(-1, resultado);
-}
-
-void testErro7(void){
-    int resultado = getTamanhoDiretorio(NULL);
-    TEST_ASSERT_EQUAL_INT(-1, resultado);
-}
-
-
-
-int main(void){
+int main(void) {
     UNITY_BEGIN();
-    
-    RUN_TEST(testCriarHash1);
-    RUN_TEST(testCriarHash2);
-    RUN_TEST(testCriarHash3);
-    RUN_TEST(testCriarHash4);
-    RUN_TEST(testCriarHash5);
-    RUN_TEST(testCriarHash6);
-    
-    RUN_TEST(testInserirItem);
-    RUN_TEST(testBuscaItem1);
-    RUN_TEST(testBuscaItem2);
-    RUN_TEST(testBuscaItem3);
-    RUN_TEST(testInserirItens);
-    RUN_TEST(testInserirItemChave);
-    
-    RUN_TEST(testDeletarItem1);
-    RUN_TEST(testDeletarItem2);
-    RUN_TEST(testDeletarItem3);
-    RUN_TEST(testDeletarItem4);
-    
-    RUN_TEST(testSplit1);
-    RUN_TEST(testSplit2);
-    RUN_TEST(testDobraDiretorio);
-    
-    RUN_TEST(testCriaBucket1);
-    RUN_TEST(testCriaBucket2);
-    RUN_TEST(testGetProfundidadeBucket);
-    RUN_TEST(testGetQuantidadeBucket);
-    RUN_TEST(testBucketEstaCheio);
-    
-    RUN_TEST(testFreeHash1);
-    RUN_TEST(testFreeHash2);
-    
-    RUN_TEST(testErro1);
-    RUN_TEST(testErro2);
-    RUN_TEST(testErro3);
-    RUN_TEST(testErro4);
-    RUN_TEST(testErro5);
-    RUN_TEST(testErro6);
-    RUN_TEST(testErro7);
-    
+
+    RUN_TEST(test_criarHF_sucesso);
+    RUN_TEST(test_criarHF_parametros_invalidos);
+    RUN_TEST(test_insertHF_sucesso);
+    RUN_TEST(test_insertHF_chave_duplicada);
+    RUN_TEST(test_insertHF_parametros_invalidos);
+    RUN_TEST(test_buscaHF_encontrado);
+    RUN_TEST(test_buscaHF_nao_encontrado);
+    RUN_TEST(test_buscaHF_parametros_invalidos);
+    RUN_TEST(test_buscaHF_buffer_insuficiente);
+    RUN_TEST(test_deletarItemHF_sucesso);
+    RUN_TEST(test_deletarItemHF_nao_encontrado);
+    RUN_TEST(test_deletarItemHF_parametros_invalidos);
+    RUN_TEST(test_refreshHF_sucesso);
+    RUN_TEST(test_refreshHF_nao_encontrado);
+    RUN_TEST(test_refreshHF_parametros_invalidos);
+    RUN_TEST(test_totalRegistrosHF);
+    RUN_TEST(test_totalRegistrosHF_NULL);
+    RUN_TEST(test_getProfGlobalHF);
+    RUN_TEST(test_getTamBucketHF);
+    RUN_TEST(test_getInfoBucketHF);
+    RUN_TEST(test_getInfoBucketHF_invalido);
+    RUN_TEST(test_insercao_multipla_com_split);
+    RUN_TEST(test_deletar_e_reinserir);
+    RUN_TEST(test_gerarDumpHF);
+    RUN_TEST(test_gerarDumpHF_NULL);
+
     return UNITY_END();
 }
